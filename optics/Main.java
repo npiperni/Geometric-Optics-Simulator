9a8be4b8ceef55c45d765a4a8559bc08f2aec706
Nick <36056036+nickuniverse91@users.noreply.github.com>
2020-11-10 21:01:07 -0500

    Add files via upload

---
+++ optics/Main.java
@@ -0,0 +1,47 @@
+package optics;
+
+import controller.MenuBarController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.stage.Stage;
+import model.MenuItems;
+import view.MenuBarView;
+
+
+public class Main extends Application {
+    private static Stage s;
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        MenuBarView mbt = new MenuBarView();
+        MenuItems mi = new MenuItems();
+        MenuBarController mbc = new MenuBarController(mi, mbt);
+
+        BorderPane bp = new BorderPane(mbt);
+
+        s = stage;
+
+        bp.getStylesheets().add("style.css");
+
+        stage.setTitle("Menu Bar Test");
+        Scene scene = new Scene(bp, 900, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static Stage getS() {
+        return s;
+    }
+
+    public static void setS(Stage s) {
+        Main.s = s;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
